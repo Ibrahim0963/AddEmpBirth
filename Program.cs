a10415a8af599ccd6a4fd42c3a44cc83d8d2d09c
Ibrahim alakeel <ibrahimalakeel@github.com>
2019-04-27 01:16:29 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddEmpBirth
+{
+    class Program
+    {
+        struct Employee
+        {
+            public string EmpName;
+            public DateOfBirth Date;
+        }
+        struct DateOfBirth
+        {
+            public int Day;
+            public int Month;
+            public int Year;
+        }
+        static int count=0;
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+
+            //  int dd = 0, mm = 0, yy = 0;
+
+            //Console.WriteLine("How many Employees do you want to add ? ");
+            //int total = int.Parse(Console.ReadLine());
+            char menu;
+            Console.Write("\n\nCreate a nested struct and store data in an array :\n");
+            Console.Write("-------------------------------------------------------\n");
+
+            // Employee[] emp = new Employee[total];
+            List<Employee> emps = new List<Employee>(); // List<Employee> emp = new List<Employee>(); without this Employee emp;
+            Employee emp;
+           do
+            {
+                Console.Write("Name of the employee : ");
+                emp.EmpName = Console.ReadLine();
+
+                Console.Write("Input day of the birth : ");
+                emp.Date.Day = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Input month of the birth : ");
+                emp.Date.Month = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Input year for the birth : ");
+                emp.Date.Year = Convert.ToInt32(Console.ReadLine());
+                emps.Add(emp);
+                // Console.WriteLine();
+                Console.WriteLine("if you want to continue clik (y) else (n): ");
+                menu = Console.ReadKey().KeyChar;
+            }
+            while (menu != 'n' && menu != 'N');
+            Console.Clear();
+            foreach (Employee E in emps)
+            {
+                Console.WriteLine("Employee Name : "+E.EmpName); // look 
+                Console.WriteLine("Employee Date : "+E.Date.Day + "/" +E.Date.Month+ "/" + E.Date.Year); // look
+                Console.WriteLine("---------------------------------------------------------------------");
+         
+            }
+            Console.ReadKey();
+        }
+    }
+}
